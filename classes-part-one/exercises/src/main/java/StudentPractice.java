2eda040c8a13a03a83d347c15e119612cab091c3
prinshaks <prinshasudha@gmail.com>
2024-10-01 22:57:03 -0500

    Java Exercise - Getter Setter

--- classes-part-one/exercises/src/main/java/StudentPractice.java
+++ classes-part-one/exercises/src/main/java/StudentPractice.java
@@ -1,5 +1,9 @@
 public class StudentPractice {
     public static void main(String[] args){
         //insantiate your Student class below
+        Student std1=new Student("prinsha", 234, 1, 4.0);
+
     }
 }
+
+
